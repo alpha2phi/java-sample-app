163cfc814664c71e3a4c3868da5a7a9bd214daff
alpha2phi <alpha2phi@gmail.com>
2022-05-28 22:04:47 +0800

    Update code

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,19 +11,17 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class DemoApplication {
 
-  public static void main(String[] args) {
+  public static void main(final String[] args) {
     SpringApplication.run(DemoApplication.class, args);
   }
 
   @Bean
-  public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+  public CommandLineRunner commandLineRunner(final ApplicationContext ctx) {
     return args -> {
-
       System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-      String[] beanNames = ctx.getBeanDefinitionNames();
+      final String[] beanNames = ctx.getBeanDefinitionNames();
       Arrays.sort(beanNames);
-      for (String beanName : beanNames) {
+      for (final String beanName : beanNames) {
         System.out.println(beanName);
       }
 
